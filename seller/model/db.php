dbe908dbaba3754b0391beed363ac1929d9c3868
Mithila <141346037+Zaman-Mithila@users.noreply.github.com>
2025-02-03 05:12:25 +0600

    Update db.php

--- seller/model/db.php
+++ seller/model/db.php
@@ -1,15 +1,14 @@
 <?php
 class db {
-    $servername = "localhost";
-     $username = "root";  
-     $password = "";      
-     $dbname = "seca"; 
 
-     function createCon() {
-        
-        $conn = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
+    function createCon(){
+        $servername = "localhost";
+        $username = "root";  
+        $password = "";      
+        $dbname = "seca";
 
-        
+        $conn = new mysqli($servername, $username, $password, $dbname);
+
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
         }
